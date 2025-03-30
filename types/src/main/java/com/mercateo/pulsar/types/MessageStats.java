ff97272b2b56146f77138408ab8b636d1b7a3384
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-03-30 20:07:00 +0200

    Cleanup

--- types/src/main/java/com/mercateo/pulsar/types/MessageStats.java
+++ types/src/main/java/com/mercateo/pulsar/types/MessageStats.java
@@ -1,4 +1,4 @@
 package com.mercateo.pulsar.types;
 
-public record MessageStats(int userId, long textCount, long lineCount) {
+public record MessageStats(int userId, long textCount, long wordCount) {
 }
